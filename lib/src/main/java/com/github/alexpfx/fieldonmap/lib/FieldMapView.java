0a96930f0f1a4fe741c8acf255cf3327dcc18b38
alexpfx <alexpfba@gmail.com>
2017-03-19 12:09:03 -0300

    adicionando maps

---
+++ lib/src/main/java/com/github/alexpfx/fieldonmap/lib/FieldMapView.java
@@ -0,0 +1,10 @@
+package com.github.alexpfx.fieldonmap.lib;
+
+import android.widget.FrameLayout;
+
+/**
+ * Created by alexandre on 19/03/2017.
+ */
+
+public class FieldMapView extends FrameLayout {
+}
