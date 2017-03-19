e64601ca1408540d994f5f9e85e2443455ee8c80
alexpfx <alexpfba@gmail.com>
2017-03-19 12:09:24 -0300

    criando compound component

--- lib/src/main/java/com/github/alexpfx/fieldonmap/lib/FieldMapView.java
+++ lib/src/main/java/com/github/alexpfx/fieldonmap/lib/FieldMapView.java
@@ -1,5 +1,9 @@
 package com.github.alexpfx.fieldonmap.lib;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 /**
@@ -7,4 +11,18 @@ import android.widget.FrameLayout;
  */
 
 public class FieldMapView extends FrameLayout {
+    private FieldDraw fieldDraw;
+
+    public FieldMapView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public void init (){
+        inflate(getContext(), R.layout.field_map_view, this);
+        fieldDraw = (FieldDraw) findViewById(R.id.field_drawer);
+
+
+    }
+
 }
