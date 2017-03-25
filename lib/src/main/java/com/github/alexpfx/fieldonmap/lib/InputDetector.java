cb1927a3179cdbe80f4b4a4fd8454c54ce57d026
alexpfx <alexpfba@gmail.com>
2017-03-24 22:30:33 -0300

    fix

--- lib/src/main/java/com/github/alexpfx/fieldonmap/lib/InputDetector.java
+++ lib/src/main/java/com/github/alexpfx/fieldonmap/lib/InputDetector.java
@@ -16,7 +16,7 @@ public class InputDetector {
     }
 
     public boolean onTouchEvent(MotionEvent ev) {
-        ev.findPointerIndex()
+
         int action = ev.getAction();
         float x = ev.getX();
         float y = ev.getY();
